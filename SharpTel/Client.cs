48c3030a8a0c471e35fa000ac3bc5355acff43f5
Eduardo <eduardoalbarado@gmail.com>
2019-11-28 15:05:34 -0300

    Send data to MSSQL

--- SharpTel/Client.cs
+++ SharpTel/Client.cs
@@ -5,11 +5,13 @@ using System.Text;
 using System.Net.Sockets;
 using System.IO;
 using System.Threading;
+using System.Data.SqlClient;
 
 namespace SharpTel{
     class Client{
 
         StreamReader input = null;
+        
 
         public Client(StreamReader input){
             this.input = input;
@@ -17,27 +19,53 @@ namespace SharpTel{
 
         public void Run(){
             String line;
+
             while ((line=input.ReadLine())!=null){
+                Conexion(line);
                 Console.Write(line+"\r\n");
             }
         }
 
+        void Conexion(string ingresar)
+        {
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=SMRD;Integrated Security=True";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            //Console.WriteLine("DATABASE connection open");
+
+            String sql = "INSERT INTO [dbo].[Registros] (registro) VALUES ( '" + ingresar.Replace("'","''") + "')";
+            using (SqlCommand command = new SqlCommand(sql, cnn))
+            {
+                command.ExecuteNonQuery();
+                //Console.WriteLine("Done.");
+            }
+
+            cnn.Close();
+            //Console.WriteLine("DATABASE connection close");
+        }
+
         static void Main(string[] args){
-                Console.Write("Host: ");
-                string host=Console.ReadLine();
-                Console.Write("Port: ");
-                int port;
-                try{
-                    port=int.Parse(Console.ReadLine());
-                }
-                catch(Exception){
-                    Console.WriteLine("Wrong parameter. Using 23 instead");
-                    port=23;
-                }
+                Console.Write("Conecting to Host: ");
+                //string host=Console.ReadLine();
+                string host="10.0.0.1" +
+                "";
+                Console.Write("Conecting to port: ");
+                //int port;
+                //try{
+                //    port=int.Parse(Console.ReadLine());
+                //}
+                //catch(Exception){
+                //    Console.WriteLine("Wrong parameter. Using 23 instead");
+                //    port=23;
+                //}
+                int port=2300;
                 TcpClient socket=null;
                 try{
                     socket = new TcpClient(host, port);
-                }
+                    Console.Write("Successful connection to PBX: ");
+                 }
                 catch(SocketException){
                     Console.WriteLine("Unknown host - " + host + ". Quitting");
                     Console.ReadKey();
@@ -51,10 +79,36 @@ namespace SharpTel{
                 Thread t = new Thread(new ThreadStart(cliobj.Run));
                 t.Start();
 
+                int login = 0;
                 while(true){
-                    output.Write(Console.ReadLine()+"\r\n");
-                    output.Flush();
+
+                switch (login)
+                {
+                    case 0:
+                        login = login + 1;
+                        output.Write("SMDR" + "\r\n");
+                        output.Flush();
+                        break;
+                    case 1:
+                        login = login + 1;
+                        output.Write("PCCSMDR" + "\r\n");
+                        output.Flush();
+                        break;
+                    default:
+                        string salida = Console.ReadLine();
+                        output.Write( salida + "\r\n");
+                        output.Flush();
+                        
+                        break;
                 }
+
+                }
+
+
         }
+
+
+
+
     }
 }
